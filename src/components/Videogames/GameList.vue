aa65c98ac712d8d7b349ae07774493ece2a238a4
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-20 18:02:16 +0100

    fixed graphic

--- src/components/Videogames/GameList.vue
+++ src/components/Videogames/GameList.vue
@@ -8,14 +8,19 @@ export default {
 
 <template>
     <div class="container my-5">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title text-center mb-0 mt-3">Titolo del gioco</h5>
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-0 mt-3">Titolo del gioco</h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-center text-warning"><strong class="text-dark">Vote: </strong>Numero
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item text-center text-warning"><strong class="text-dark">Vote: </strong>Numero</li>
-            </ul>
         </div>
     </div>
 </template>
