c4bdfdde0a47737a4704b34ff27e088d60e6abfe
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-20 17:40:31 +0100

    ajax call in homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,51 @@
 <script>
+import VideoGameDetails from './VideoGameDetails.vue';
 import axios from 'axios'
+const apiBaseUrl = 'http://localhost:8000/api/';
 export default {
-name:'HomePage'
+    name: 'HomePage',
+    components: { VideoGameDetails },
+    data() {
+        return {
+            videogames: [],
+            isLoading: false,
+            hasError: false,
+        }
+    },
+    methods: {
+        fetchVideoGames() {
+            this.isLoading = true;
+            axios.get(apiBaseUrl + 'videogames').then(res => {
+                this.videogames = res.data;
+            }).catch(() => {
+                this.hasError = true;
+            }).then(() => {
+                this.isLoading = false;
+            })
+        }
+    },
+    created() {
+        this.fetchVideoGames();
+    }
 }
 </script>
 
 
 <template>
+    <app-loader v-if="isLoading"></app-loader>
 
+    <!-- videogames -->
+    <div v-else class="container">
+        <error-alert :hasError="hasError" @close="hasError = false"></error-alert>
 
+        <video-game-details v-if="videogames.length" :videogames="videogames"></video-game-details>
+
+        <div v-else class="text-center mt-5">
+            <h2>There aren't projects to show.</h2>
+        </div>
+
+    </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
