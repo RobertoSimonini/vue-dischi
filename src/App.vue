69bbaaf09f4161c89647fc3882795916ed22cbb8
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-20 16:36:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+export default {
+
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+
+<style lang="scss">
+  @use './assets/styles/style.scss';
+</style>  
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from '../pages/HomePage.vue';
+import ContactPage from '../pages/ContactPage.vue';
+import VideoGameDetails from '../pages/ProjectDetails.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
+
+const router = createRouter({
+    linkExactActiveClass : 'active',
+    history: createWebHistory(),
+    routes: [
+        {path:'/',  name: 'home', component: HomePage},
+        {path:'/contact',  name: 'contact', component: ContactPage},
+        {path:'/videogames/:id',  name: 'videogame-details', component: VideoGameDetails},
+        { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage }
+    ]
+});
+
+export { router };
